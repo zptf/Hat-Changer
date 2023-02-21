0d73d19f102fea01b2e0f0f05a97fb34c60b3ffe
zap <109105581+zaplec@users.noreply.github.com>
2023-02-21 16:48:58 +0000

    Update source.lua

--- source.lua
+++ source.lua
@@ -1890,7 +1890,7 @@ function ThunderMadeLibrary:CreateWindow(Settings)
 			UserInputService.InputBegan:Connect(function(input, processed)
 
 				if CheckingForKey then
-					if input.KeyCode ~= Enum.KeyCode.Unknown and input.KeyCode ~= Enum.KeyCode.End then
+					if input.KeyCode ~= Enum.KeyCode.Unknown and input.KeyCode ~= Enum.KeyCode.Delete then
 						local SplitMessage = string.split(tostring(input.KeyCode), ".")
 						local NewKeyNoEnum = SplitMessage[3]
 						Keybind.KeybindFrame.KeybindBox.Text = tostring(NewKeyNoEnum)
@@ -2311,7 +2311,7 @@ Topbar.Hide.MouseButton1Click:Connect(function()
 end)
 
 UserInputService.InputBegan:Connect(function(input, processed)
-	if (input.KeyCode == Enum.KeyCode.End and not processed) then
+	if (input.KeyCode == Enum.KeyCode.Delete and not processed) then
 		if Debounce then return end
 		if Hidden then
 			Hidden = false
